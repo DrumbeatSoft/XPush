02c61c730aca36cebb09e3f46277aac6c84110b6
wangyu <282729478@qq.com>
2020-05-25 09:51:26 +0800

    合并aar4

--- xpush-oppo/src/main/java/com/xuexiang/xpush/oppo/net/OppoPushAPI.java
+++ xpush-oppo/src/main/java/com/xuexiang/xpush/oppo/net/OppoPushAPI.java
@@ -1,6 +1,7 @@
 package com.xuexiang.xpush.oppo.net;
 
 public interface OppoPushAPI {
+    String baseUrl = "http://192.168.70.99:20002/push";
     String auth = baseUrl + "/oppoPushController/getOppoProjectAuthToken";
     String updateDeviceTagAlias = baseUrl + "/oppoPushController/updateDeviceTagAlias";
     String getDeviceTagAlias = baseUrl + "/oppoPushController/getDeviceTagAlias";
